c5b2eaff3784317f481cf682c54de64cf75c0291
Schuyler Martin <schuylermartin45@gmail.com>
2022-11-17 17:59:30 -0700

    Finished v1.0
    - Initial test pattern draws. That's it, that's the program. It's done!

--- src/gb_test_screen_alignment.c
+++ src/gb_test_screen_alignment.c
@@ -13,6 +13,9 @@ Description:    Test program that can be used to validate the alignment of a
 #include <stdio.h>
 #include <stdbool.h>
 
+#include "pattern.h"
+#include "tileset.h"
+
 /***** Functions *****/
 
 /*
@@ -20,7 +23,15 @@ Description:    Test program that can be used to validate the alignment of a
 ** @return Exit code...for historical reasons and to be "proper"
 */
 int main() {
+    set_bkg_data(0, 9, PatternTiles);
+    set_bkg_tiles(0, 0, patternWidth, patternHeight, pattern);
+
+    // Render data and turn the screen on
+    SHOW_BKG;
+    DISPLAY_ON;
+
     while (true) {
+        // TODO add more features in the future(?)
         wait_vbl_done();
     }
 
